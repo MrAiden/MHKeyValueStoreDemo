8bd0be7c4397bd0b1bd0dfd69ded4eaa90508cf1
Mortar <yan_yan.vip@foxmail.com>
2018-11-26 09:38:16 +0800

    新建 Demo 项目

---
+++ MHKeyValueStoreDemo/MHKeyValueStoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MHKeyValueStoreDemo
+//
+//  Created by Mortar on 2018/11/26.
+//  Copyright © 2018 Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MHKeyValueStoreDemo/MHKeyValueStoreDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MHKeyValueStoreDemo
+//
+//  Created by Mortar on 2018/11/26.
+//  Copyright © 2018 Yan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
